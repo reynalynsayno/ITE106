1b0dc648c1239eac5005bf8a71339f28acba6b06
reynalynsayno <saynoreynalyn1807@gmail.com>
2024-10-15 16:40:40 +0800

    Add files via upload

---
+++ TestCase01Sayno.java
@@ -0,0 +1,22 @@
+public class TestCase01Sayno {
+	public static void main(String[] args) {
+	
+	String Grade;
+		if (Grade => 90) {
+			System.out.println("A");
+		}
+		else if (Grade => 80) {
+			System.out.println("B");
+		}
+		else if (Grade => 70) {
+			System.out.println("C");
+		}
+		else if (Grade => 60) {
+			System.out.println("D");
+		}
+		else {
+			System.out.println("F");
+		}
+		
+}
+}
